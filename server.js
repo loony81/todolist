30129076ddd6614abb99bff162eac85409da919c
Roman Nazarenko <r.nazarenko.81@gmail.com>
2021-11-02 17:06:49 +0200

    connect to mongodb

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const winston = require('winston') // a package for logging exceptions
 const config = require('config')
 const Joi = require('joi') //a package for data validation in Node/MongoDB applications
 Joi.objectId = require('joi-objectid')(Joi) // adds id validation to Joi
+const mongoose = require('mongoose')
 const app = express()
 
 
 const port = config.get('port')
-console.log(config.get('db'))
-console.log(config.get('jwtkey'))
+const db = config.get('db')
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log(`connected to ${db} ...`))
+	.catch(err => console.error(`could not connect to MongoDB: ${err}`))
 
 //middleware
 app.use(helmet())
