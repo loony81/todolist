376c3798be57131463e2e7e1ade851b823222a6f
Roman Nazarenko <r.nazarenko.81@gmail.com>
2021-11-10 18:25:10 +0200

    rewrite routes/todos.js so that it is now works with a particular user's todos

--- routes/todos.js
+++ routes/todos.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const {Todo, validate} = require('../models/todo')
+const {User} = require('../models/user')
 const auth = require('../middleware/auth')
 
 router.get('/', auth, async (req, res) => {
-	const todos = await Todo.find()
+	// get user's id from jwt payload, find him in the database and get his todos
+	const todos  = await User.findById(req.user._id).select('todos')
 	res.json(todos)
 })
  // this route is unnecessary but I included it for the sake of conformity with the REST api
 router.get('/:id', auth, async (req, res) => {
-	const todo = await Todo.findById(req.params.id)
+	const user  = await User.findById(req.user._id).select('todos -_id')
+	const todo = user.todos.id(req.params.id)
 	if (!todo) return res.status(404).send('The todo with the given ID was not found.')
   	res.json(todo)
 })
@@ -17,28 +20,35 @@ router.get('/:id', auth, async (req, res) => {
 router.post('/', auth, async (req, res) => {
 	const {error} = validate(req.body)
 	if(error) return res.status(400).send(error.details[0].message)
+	const user  = await User.findById(req.user._id)
 	const todo = new Todo({
 		name: req.body.name,
 		completed: req.body.completed
 	})
-	await todo.save()
-	res.status(201).json(todo)
+	user.todos.push(todo)
+	await user.save()
+	res.status(201).json(user.todos)
 })
 
 router.put('/:id', auth, async (req, res) => {
-  const todo = await Todo.findById(req.params.id)
-  if (!todo) return res.status(404).send('The todo with the given ID was not found.')
-  const { error } = validate(req.body);
+  const { error } = validate(req.body)
   if(error) return res.status(400).send(error.details[0].message)
+  const user  = await User.findById(req.user._id)
+  const todo = user.todos.id(req.params.id)
+  if (!todo) return res.status(404).send('The todo with the given ID was not found.')
   todo.name = req.body.name
   todo.completed = req.body.completed || todo.completed
-  await todo.save()
+  await user.save()
+  
   res.status(201).json(todo)
 })
 
 router.delete('/:id', auth, async (req,res) => {
-	const result = await Todo.findByIdAndRemove(req.params.id)
-	if (!result) return res.status(404).send('The todo with the given ID was not found.');
+	const user  = await User.findById(req.user._id)
+	const todo = user.todos.id(req.params.id)
+	if (!todo) return res.status(404).send('The todo with the given ID was not found.')
+    const result = user.todos.pull(req.params.id)
+	user.save()
 	res.json(result)
 })
 
