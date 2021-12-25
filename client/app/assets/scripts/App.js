839565478db7ea509f355dfaf1e31844714bfdac
Roman Nazarenko <r.nazarenko.81@gmail.com>
2021-12-25 20:05:00 +0200

    install jwt-check-expiration package to check jwt expiration on the client side

--- client/app/assets/scripts/App.js
+++ client/app/assets/scripts/App.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter as Router} from 'react-router-dom'
 import regeneratorRuntime from "regenerator-runtime"
+import { isJwtExpired } from 'jwt-check-expiration'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -20,10 +21,14 @@ const App = () => {
 
 	//when the app is loaded for the first time or the page is just reloaded, 
 	//we need to check if a user is still authenticated since the previous session
-
+	// the jwt-check-expiration package allows to check if the jwt is expired or not
+	// locally without sending a request to the server 
 	useEffect(() => {
 		const token = localStorage.getItem('x-auth-token')
-		if(token) setIsAuthenticated(true)
+		if(token){
+			if(isJwtExpired(localStorage.getItem('x-auth-token'))) {
+			} else setIsAuthenticated(true)
+		}
 	}, [])
 
 	return (
